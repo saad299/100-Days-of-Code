05dee99d43155b06e6d4ce2b8dadfb3cf4768c4d
saad299 <msaadahmad421@gmail.com>
2026-02-02 02:11:17 +0500

    day 1

---
+++ Day_1/day1/src/App.jsx
@@ -0,0 +1,18 @@
+import Counter from "./Counter";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-gray-200">
+        <div className="text-yellow-800 text-4xl font-extrabold">
+          <h1 className="text-center underline decoration-pink-500">Day 1: React Basic Concepts Refresher</h1>
+        </div>
+        <br />
+        <Counter />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Day_1/day1/src/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className="text-center space-y-4">
+        <h1>Counter Component</h1>
+        <p className="font-extrabold text-amber-500">Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(count - 1)}>Decrease</button>
+      </div>
+      <div className="text-center">
+        <h2>onChange example</h2>
+        <input type="text" value={name} onChange={handleInputChange} className="border-2 rounded-lg border-amber-600"/>
+        <p>The text is: {name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
