cbfba316a605e121e5f8894f1a3ec557a26cf7dc
saad299 <msaadahmad421@gmail.com>
2026-02-17 22:40:37 +0500

    day 16

---
+++ Days_11-20/react_advanced/src/components/TourProject/pages/Home.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <>
+      <nav>
+        <p>Home</p>
+        <p>About</p>
+        <p>Services</p>
+        <p>Packages</p>
+        <p>Contact Us</p>
+      </nav>
+    </>
+  );
+};
+
+export default Home;
