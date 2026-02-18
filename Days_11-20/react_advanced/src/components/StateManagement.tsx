50b5a25aaec06276805d79c7e4cc02ac2bfefdc9
saad299 <msaadahmad421@gmail.com>
2026-02-18 20:06:09 +0500

    day 17

--- Days_11-20/react_advanced/src/components/StateManagement.tsx
+++ Days_11-20/react_advanced/src/components/StateManagement.tsx
@@ -1,9 +1,7 @@
 const StateManagement = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">
-        Day11: State Management
-      </h1>
+      <h1 className="text-3xl font-bold">Day11: State Management</h1>
       <p>
         State Management is the process and technique of maintaining and
         managing states in a component.
@@ -45,7 +43,10 @@ const StateManagement = () => {
         we can add all the necessary states that will be needed throughout the
         application.
       </p>
-      <p>It's working is very similar to Context API, except that it doesn't require context wrappers.</p>
+      <p>
+        It's working is very similar to Context API, except that it doesn't
+        require context wrappers.
+      </p>
 
       <h2 className="text-3xl font-bold text-sky-800">Day13: Tanstack Query</h2>
       <p>
@@ -56,11 +57,56 @@ const StateManagement = () => {
       <p>
         Server state is basically a state that is coming from the server (APIs).
       </p>
-      <p>For managing a state that is coming from the server, TanStack Query is preferably used.</p>
+      <p>
+        For managing a state that is coming from the server, TanStack Query is
+        preferably used.
+      </p>
 
-      <h2 className="text-3xl font-bold">Day14: Pagination + Infinite Scroll</h2>
-      <p>It is the technique of loading more data when the user scrolls down the page.</p>
-      <p>Either by <code>Pagination</code> where we load more data when the user clicks on the page number, or by <code>Infinite Scroll</code> where we load more data when the user scrolls down the page.</p>
+      <h2 className="text-3xl font-bold">
+        Day14: Pagination + Infinite Scroll
+      </h2>
+      <p>
+        It is the technique of loading more data when the user scrolls down the
+        page.
+      </p>
+      <p>
+        Either by <code>Pagination</code> where we load more data when the user
+        clicks on the page number, or by <code>Infinite Scroll</code> where we
+        load more data when the user scrolls down the page.
+      </p>
+
+      <h2 className="text-3xl font-bold text-purple-700">
+        Day15: React Router
+      </h2>
+      <p>React Router is a library for routing in React.</p>
+      <p>
+        It is used to navigate between different pages in a React application.
+      </p>
+      <p>There are two approaches to use React Router.</p>
+      <ol>
+        <li>Component Based Routing</li>
+        <li>Data Based Routing</li>
+      </ol>
+      <p>Component Based Routing is the most common but old approach.</p>
+      <p>
+        Data Based Routing is the new and modern approach introduced in React
+        Router v6.4+.
+      </p>
+
+      <h2 className="text-3xl font-bold text-green-700">
+        Day17: Forms with libraries
+      </h2>
+      <p>There are many libraries for handling forms in React.</p>
+      <p>Some of the popular libraries are:</p>
+      <ul>
+        <li>Formik</li>
+        <li>React Hook Form</li>
+        <li>Final Form</li>
+      </ul>
+      <p>
+        These libraries provide a lot of features for handling forms in React.
+      </p>
+      <p>They provide features like validation, error handling, and more.</p>
     </div>
   );
 };
--- Days_11-20/react_advanced/src/components/TourProject/pages/Home.tsx
+++ Days_11-20/react_advanced/src/components/TourProject/pages/Home.tsx
@@ -5,9 +5,108 @@ const Home = () => {
         <p>Home</p>
         <p>About</p>
         <p>Services</p>
-        <p>Packages</p>
+        <p>Top Attractions</p>
+        <p>Tour Packages</p>
         <p>Contact Us</p>
       </nav>
+      <div>
+        <h2 className="text-3xl font-bold">APIs to use:</h2>
+        <p>https://amadeus.com</p>
+        <p>Showing countries and cities:</p>
+        <p>https://restcountries.com/</p>
+        <p>Showing images:</p>
+        <p>https://unsplash.com/developers</p>
+        <p>Showing weather:</p>
+        <p>https://openweathermap.org/api</p>
+        <p>Showing location:</p>
+        <p>https://opentripmap.io/</p>
+        <p>Showing places:</p>
+        <p>https://www.geoapify.com/</p>
+        <p>Exchange rate:</p>
+        <p>https://www.exchangerate-api.com/</p>
+      </div>
+
+      <div>
+        <h2 className="text-3xl font-bold">Homepage Structure</h2>
+        <ol>
+          <li>Hero section</li>
+          <li>Why choose us section</li>
+          <li>Our Services section</li>
+          <li>Top Attractions preview section</li>
+          <li>Featured Tours/Experiences</li>
+          <li>How It Works section</li>
+          <li>Customer Testimonials section</li>
+          <li>Contact Us/Inquiry section</li>
+          <li>Footer with contact info and social media links section</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2 className="text-3xl font-bold">About page Structure</h2>
+        <ol>
+          <li>Our Story section</li>
+          <li>Our Mission section</li>
+          <li>What we offer section</li>
+          <li>What choose us section</li>
+          <li>Our Team section</li>
+          <li>Our Values section</li>
+          <li>Call to action/Contact Us/Inquiry section</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2 className="text-3xl font-bold">Services page Structure</h2>
+        <ol>
+          <li>Tour Packages section</li>
+          <li>Experiences section</li>
+          <li>Destinations section</li>
+          <li>Call to action/Contact Us/Inquiry section</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2 className="text-3xl font-bold">Services page Structure</h2>
+        <ol>
+          <li>Tour Packages section</li>
+          <li>Experiences section</li>
+          <li>Destinations section</li>
+          <li>Call to action/Contact Us/Inquiry section</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2 className="text-3xl font-bold">Top Attractions page Structure</h2>
+        <ol>
+          <li>Search and Filter section</li>
+          <li>Featured Attraction of the Month Section</li>
+          <li>Main Attractions Grid section</li>
+          <li>Categories Section</li>
+          <li>Call to action/Contact Us/Inquiry section</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2 className="text-3xl font-bold">Tour Packages page Structure</h2>
+        <ol>
+          <li>Search and Filter section</li>
+          <li>Popular Packages section</li>
+          <li>Packages Categories section</li>
+          <li>Full Package Details section</li>
+          <li>Deals/Seasonal Offers section</li>
+          <li>Call to action/Contact Us/Inquiry section</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2 className="text-3xl font-bold">Contact Us page Structure</h2>
+        <ol>
+          <li>Contact Form section</li>
+          <li>Contact Information section</li>
+          <li>Map and Location section</li>
+          <li>FAQs section</li>
+          <li>Call to action/Contact Us/Inquiry section</li>
+        </ol>
+      </div>
     </>
   );
 };
