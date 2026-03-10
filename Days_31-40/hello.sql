42ed5ff9f56fcc573035e0f5b8a3182271807cbb
saad299 <msaadahmad421@gmail.com>
2026-03-11 04:36:58 +0500

    day 37: Update database documentation with password hashing details and refine SQL insert examples

--- Days_31-40/hello.sql
+++ Days_31-40/hello.sql
@@ -10,8 +10,15 @@
 -- -- Insert data
 -- INSERT INTO users (name, email, age)
 -- VALUES ('Alice', 'alice@email.com', 28);
+-- VALUES ('Saad', 'saad@email.com', 28);
+-- VALUES ('John', 'john@email.com', 28);
+-- VALUES ('Joe', 'joe@email.com', 30);
 
 -- Reading data
-SELECT TOP 10 name, email FROM users WHERE age > 25 ORDER BY name ASC
+SELECT TOP 10
+  name, email
+FROM users
+WHERE age > 25
+ORDER BY name ASC
 
 SELECT 'Hello from Computer B!' AS Message
