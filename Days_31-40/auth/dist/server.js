3be4b40d7c97eb0014cdbfb68a5e78e23cfd8638
saad299 <msaadahmad421@gmail.com>
2026-03-23 00:15:39 +0500

    day 48: Refactor project structure and configuration for improved environment handling and module resolution

--- Days_31-40/auth/dist/server.js
+++ Days_31-40/auth/dist/server.js
@@ -13,14 +13,17 @@ const auth_route_1 = __importDefault(require("./routes/auth.route"));
 const user_routes_1 = __importDefault(require("./routes/user.routes"));
 const db_1 = __importDefault(require("./config/db"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 (0, db_1.default)();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use("/api/auth", auth_route_1.default);
 app.use("/api/users", user_routes_1.default);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+exports.default = app;
 //# sourceMappingURL=server.js.map
