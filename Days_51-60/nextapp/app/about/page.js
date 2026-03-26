91b01963ef18abff3f5f79b6be2130544a223b05
saad299 <msaadahmad421@gmail.com>
2026-03-26 10:08:38 +0500

    day 52

--- Days_51-60/nextapp/app/about/page.js
+++ Days_51-60/nextapp/app/about/page.js
@@ -1,4 +1,4 @@
-const page = () => {
+const about = () => {
   return (
     <>
     <div>
@@ -8,4 +8,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default about;
--- Days_51-60/nextapp/components/Header.jsx
+++ Days_51-60/nextapp/components/Header.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 const Header = () => {
   return (
     <>
@@ -5,6 +6,19 @@ const Header = () => {
         <h1 className="text-center text-3xl font-bold bg-amber-400">
           This is a Header
         </h1>
+      </div>
+      <div>
+        <ul>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/about">
+            <li>About</li>
+          </Link>
+          <Link href="/contact">
+            <li>Contact</li>
+          </Link>
+        </ul>
       </div>
     </>
   );
