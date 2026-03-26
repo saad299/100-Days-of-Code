e68e93929ed1defa4010d83616e1d8b2e5e667ec
saad299 <msaadahmad421@gmail.com>
2026-03-26 10:08:38 +0500

    day 52

--- Days_51-60/nextapp/app/contact/page.js
+++ Days_51-60/nextapp/app/contact/page.js
@@ -1,4 +1,4 @@
-const page = () => {
+const contact = () => {
   return (
     <>
       <div>This is a homepage</div>
@@ -6,4 +6,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default contact;
