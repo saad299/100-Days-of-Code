0e5338d59ff2ead75a43be839db1432577f07de0
saad299 <msaadahmad421@gmail.com>
2026-04-01 09:43:47 +0500

    day 58: connecting db

--- Days_51-60/nextapp/db/mongodb.js
+++ Days_51-60/nextapp/db/mongodb.js
@@ -1,11 +1,14 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
 let clientPromise;
 
 if (!global._mongoClientPromise) {
   global._mongoClientPromise = client.connect();
 }
 clientPromise = global._mongoClientPromise;
+console.log("MongoDB connected");
 
 export default clientPromise;
