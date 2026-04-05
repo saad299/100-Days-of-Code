3276b1c943fc3639e005dcf5bdf14e0a8a793235
saad299 <msaadahmad421@gmail.com>
2026-04-05 14:07:48 +0500

    day 62: Python Part 2

---
+++ Days_61-70/advanced.py
@@ -0,0 +1,16 @@
+## Classes and Objects
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def greet(self):
+        print("Hello, my name is", self.name)
+    
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+    
+person = Person("Saad", 25)
+print(person)
+person.greet()
