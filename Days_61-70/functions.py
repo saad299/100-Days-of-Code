cdfb5eddd17292b6e8e8ebc6b76fd00b374a9d52
saad299 <msaadahmad421@gmail.com>
2026-04-06 10:03:44 +0500

    day 63: Python Functions

---
+++ Days_61-70/functions.py
@@ -0,0 +1,21 @@
+print("Functions")
+print("-" * 20)
+
+
+
+def greet():
+    print("Hello")
+
+greet()
+
+
+def greet(name):
+    print("Hello", name)
+
+greet("Saad")
+
+
+def add(a, b):
+    return a + b
+
+print(add(5, 3))
