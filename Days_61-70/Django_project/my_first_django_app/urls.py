e556ec2ec5ab1866c46c1b7669f1bb5b997e568e
saad299 <msaadahmad421@gmail.com>
2026-04-11 02:22:14 +0500

    day 67

--- Days_61-70/Django_project/my_first_django_app/urls.py
+++ Days_61-70/Django_project/my_first_django_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "my_first_django_app"
+
 urlpatterns = [
-    path('', views.hello, name='hello'),
+    path("", views.home, name="home"),
+    path("app/", views.app, name="app"),
+    path("about/", views.about, name="about"),
 ]
