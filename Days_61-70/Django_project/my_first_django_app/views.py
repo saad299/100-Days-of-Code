37ffacd7f57341b35e6d7c6f8157f684f6787683
saad299 <msaadahmad421@gmail.com>
2026-04-11 02:22:14 +0500

    day 67

--- Days_61-70/Django_project/my_first_django_app/views.py
+++ Days_61-70/Django_project/my_first_django_app/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
-def hello(request):
+def app(_):
     return HttpResponse("<h1>Hello from app</h1>")
+
+
+def home(request):
+    data = {"name": "John", "message_count": 5}
+    return render(request, "home.html", data)
+
+
+def about(request):
+    data = {"author": "John Doe"}
+    return render(request, "about.html", data)
