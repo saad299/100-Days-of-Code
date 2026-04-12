87820ad5729e6cff1b0f50450b0101fd550f01ec
saad299 <msaadahmad421@gmail.com>
2026-04-12 13:17:52 +0500

    day 68: Django Forms

--- Days_61-70/Django_project/my_first_django_app/urls.py
+++ Days_61-70/Django_project/my_first_django_app/urls.py
@@ -7,6 +7,13 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("app/", views.app, name="app"),
     path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     path("posts/", views.posts, name="posts"),
     path("posts/<int:id>/", views.post, name="post"),
+    # path("post/create/", views.create_post, name="create_post"),
+    # path("posts/<int:id>/update/", views.update_post, name="update_post"),
+    # path("posts/<int:id>/delete/", views.delete_post, name="delete_post"),
+    path('posts/create/', views.create_post, name='create_post'),
+    path('posts/update/<int:id>/', views.update_post, name='update_post'),
+    path('posts/delete/<int:id>/', views.delete_post, name='delete_post'),
 ]
