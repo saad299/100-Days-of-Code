5c9031bf70125179d1c80097fbb5a42e9e77df2c
saad299 <msaadahmad421@gmail.com>
2026-04-12 13:17:52 +0500

    day 68: Django Forms

--- Days_61-70/Django_project/my_first_django_app/models.py
+++ Days_61-70/Django_project/my_first_django_app/models.py
@@ -9,3 +9,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} — {self.subject}"
