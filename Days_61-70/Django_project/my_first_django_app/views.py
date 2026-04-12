93da5f23e7b9060a9c86589d028418e7dd0828c1
saad299 <msaadahmad421@gmail.com>
2026-04-12 13:17:52 +0500

    day 68: Django Forms

--- Days_61-70/Django_project/my_first_django_app/views.py
+++ Days_61-70/Django_project/my_first_django_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Contact
+from .forms import PostForm
 
 
 # Create your views here.
@@ -10,18 +11,64 @@ def app(_):
 
 def home(request):
     data = {"name": "John", "message_count": 5}
-    posts = Post.objects.all().order_by("-created_at")
+    # posts = Post.objects.all().order_by("-created_at")
     return render(request, "home.html", data)
+
 
 def posts(request):
     posts = Post.objects.all().order_by("-created_at")
     return render(request, "posts.html", {"posts": posts})
 
+
 def post(request, id):
-    post = Post.objects.get(id=id)
+    # post = Post.objects.get(id=id)
+    post = get_object_or_404(Post, id=id)
     return render(request, "post.html", {"post": post})
+
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("my_first_django_app:posts")
+    else:
+        form = PostForm()
+    return render(request, "create_post.html", {"form": form})
+
+
+def update_post(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("my_first_django_app:posts")
+    else:
+        form = PostForm(instance=post)
+    return render(request, "update_post.html", {"form": form, "post": post})
+
+
+def delete_post(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == "POST":
+        post.delete()
+        return redirect("my_first_django_app:posts")
+    return render(request, "delete_post.html", {"post": post})
 
 
 def about(request):
     data = {"author": "John Doe"}
     return render(request, "about.html", data)
+
+
+def contact(request):
+    if request.method == "POST":
+        Contact.objects.create(
+            name=request.POST["name"],
+            email=request.POST["email"],
+            subject=request.POST["subject"],
+            message=request.POST["message"],
+        )
+        return redirect("my_first_django_app:contact")
+    return render(request, "contact.html")
