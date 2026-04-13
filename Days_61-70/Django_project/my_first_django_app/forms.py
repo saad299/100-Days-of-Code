046a769de8113c7dd42a6aba2ea4ca1e5c7f24bd
saad299 <msaadahmad421@gmail.com>
2026-04-13 20:06:43 +0500

    Day 69: Authentication in Django

--- Days_61-70/Django_project/my_first_django_app/forms.py
+++ Days_61-70/Django_project/my_first_django_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from .models import Post
 
 class PostForm(forms.ModelForm):
@@ -9,3 +10,17 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+class RegisterUser(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
+class LoginUser(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- Days_61-70/Django_project/my_first_django_app/views.py
+++ Days_61-70/Django_project/my_first_django_app/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login, authenticate
 from .models import Post, Contact
-from .forms import PostForm
+from .forms import PostForm, RegisterUser, LoginUser
 
 
 # Create your views here.
@@ -72,3 +73,32 @@ def contact(request):
         )
         return redirect("my_first_django_app:contact")
     return render(request, "contact.html")
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterUser(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("my_first_django_app:posts")
+    else:
+        form = RegisterUser()
+    return render(request, "register.html", {"form": form})
+
+def loginUser(request):
+    if request.method == "POST":
+        form = LoginUser(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("my_first_django_app:posts")
+            else:
+                return render(request, "login.html", {"form": form, "error": "Invalid credentials"})
+        else:
+            return render(request, "login.html", {"form": form})
+    else:
+        form = LoginUser()
+        return render(request, "login.html", {"form": form})
