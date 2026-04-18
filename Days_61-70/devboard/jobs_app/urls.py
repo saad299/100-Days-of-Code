4851159637109725f140703f370042928cee24d8
saad299 <msaadahmad421@gmail.com>
2026-04-18 22:20:30 +0500

    Day 74: devboard dashboard

--- Days_61-70/devboard/jobs_app/urls.py
+++ Days_61-70/devboard/jobs_app/urls.py
@@ -1,4 +1,13 @@
 # jobs/urls.py
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.job_list, name="job_list"),
+    path('dashboard/', views.employer_dashboard, name="employer_dashboard"),
+    path('post/', views.create_job, name="post_job"),
+    path('edit/<int:id>/', views.edit_job, name="edit_job"),
+    path('delete/<int:id>/', views.delete_job, name="delete_job"),
+    path('applications/<int:id>/', views.application_list, name="job_applications"),
+    path('applications/status/<int:id>/', views.update_application_status, name="update_application_status"),
+]
