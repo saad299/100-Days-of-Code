b8ef0d7604282600bb8f155335e090725a8bdc07
saad299 <msaadahmad421@gmail.com>
2026-04-23 22:54:34 +0500

    day 78: devboard  jobs_app urls, views and templates

--- Days_61-70/devboard/jobs_app/urls.py
+++ Days_61-70/devboard/jobs_app/urls.py
@@ -6,12 +6,13 @@ from . import views
 app_name = 'jobs_app'
 
 urlpatterns = [
-    path('', views.jobs_applications, name="jobs_applications"),
+    # Previous ==> path('', views.jobs_applications, name="jobs_applications"),
+    path('', views.job_list, name="job_list"),
     path('dashboard', views.employer_dashboard, name="employer_dashboard"),
-    path('post', views.create_job, name="post_job"),
-    path('edit/<int:id>', views.edit_job, name="edit_job"),
-    path('delete/<int:id>', views.delete_job, name="delete_job"),
-    path('applications/<int:id>', views.application_list, name="job_applications"),
-    path('applications/status/<int:id>', views.update_application_status, name="update_application_status"),
-    path('candidate/dashboard', views.candidate_dashboard, name="candidate_dashboard"),
+    path('jobs/post', views.create_job, name="post_job"),
+    path('jobs/edit/<int:id>', views.edit_job, name="edit_job"),
+    path('jobs/delete/<int:id>', views.delete_job, name="delete_job"),
+    path('jobs/applications/<int:id>', views.job_applications, name="job_applications"),
+    path('jobs/applications/status/<int:id>', views.update_application_status, name="update_application_status"),
+    path('dashboard/candidate', views.candidate_dashboard, name="candidate_dashboard"),
 ]
