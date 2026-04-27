fb050a7ad2efc77d08e4cd03395b57d7416155fd
saad299 <msaadahmad421@gmail.com>
2026-04-27 23:37:30 +0500

    day 82: CRUD + API routes in Flask

---
+++ Days_71-80/flask-app/model.py
@@ -0,0 +1,9 @@
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    
+    def __repr__(self):
+        return f'<User {self.name}>'
