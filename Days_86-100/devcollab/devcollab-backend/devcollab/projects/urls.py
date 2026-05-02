700bb9dedd128d34c2ac239107bde6a7354935ee
saad299 <msaadahmad421@gmail.com>
2026-05-02 14:41:23 +0500

    day 89: devcollab: working on accounts app

---
+++ Days_86-100/devcollab/devcollab-backend/devcollab/projects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    # path('', views.ProjectListCreateView.as_view(), name='project-list-create'),
+    # path('<int:pk>/', views.ProjectDetailView.as_view(), name='project-detail'),
+]
