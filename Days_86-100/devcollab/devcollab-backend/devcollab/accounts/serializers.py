4744d09b75c0c748a95edc5bd1ef3958f3899b5a
saad299 <msaadahmad421@gmail.com>
2026-05-06 22:39:52 +0500

    day 91: next.js frontend starts

--- Days_86-100/devcollab/devcollab-backend/devcollab/accounts/serializers.py
+++ Days_86-100/devcollab/devcollab-backend/devcollab/accounts/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import User, Profile
 
-class RegisterSerializer:
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'password2']
@@ -9,30 +11,33 @@ class RegisterSerializer:
             'password': {'write_only': True}
         }
 
-    def validate_password(self, password):
-        if len(password) < 6:
-            raise serializers.ValidationError("Password must be at least 6 characters long.")
-        if password == password2:
-            raise serializers.ValidationError("Passwords do not match.")
-        return password
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        if len(data['password']) < 6:
+            raise serializers.ValidationError({"password": "Password must be at least 6 characters long"})
+        return data
 
     def create(self, validated_data):
+        validated_data.pop('password2')
         password = validated_data.pop('password')
-        password2 = validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         user.set_password(password)
         user.save()
         return user
 
-class ProfileSerializer:
-    model = Profile
-    fields = ['bio', 'location', 'avatar', 'skills', 'github_url', 'linkedin_url', 'website_url']
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'location', 'avatar', 'skills', 'github_url', 'linkedin_url', 'website_url']
+    
     get_skills_list = serializers.SerializerMethodField()
     
     def get_skills_list(self, obj):
         return obj.skills.split(',')
 
-class UserSerializer:
-    model = User
-    fields = ['id', 'username', 'email', 'profile']
-    depth = 1
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'profile']
+        depth = 1
