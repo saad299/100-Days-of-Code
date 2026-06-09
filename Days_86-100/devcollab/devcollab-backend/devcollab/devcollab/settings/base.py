4201bf8596dd212c2f1018d77d8675b5f6942483
saad299 <msaadahmad421@gmail.com>
2026-06-10 01:12:11 +0500

    fixed token blacklisting issue

--- Days_86-100/devcollab/devcollab-backend/devcollab/devcollab/settings/base.py
+++ Days_86-100/devcollab/devcollab-backend/devcollab/devcollab/settings/base.py
@@ -89,5 +89,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=42),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
 }
