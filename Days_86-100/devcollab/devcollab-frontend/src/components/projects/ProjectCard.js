b83140ff966cc03ef57e5886d28859c66377e192
saad299 <msaadahmad421@gmail.com>
2026-06-17 01:12:10 +0500

    DevCollab: corrected formatting

--- Days_86-100/devcollab/devcollab-frontend/src/components/projects/ProjectCard.js
+++ Days_86-100/devcollab/devcollab-frontend/src/components/projects/ProjectCard.js
@@ -1,39 +1,51 @@
-import Link from 'next/link'
-import TechStackTag from './TechStackTag'
-import RoleTag from './RoleTag'
+import Link from "next/link";
+import TechStackTag from "./TechStackTag";
+import RoleTag from "./RoleTag";
 
 function ProjectCard({ project }) {
-    const {id, title, description, owner_data, tech_stack_list, roles_list, created_at} = project;
+  const {
+    project_id,
+    title,
+    description,
+    owner_data,
+    tech_stack_list,
+    roles_list,
+    created_at,
+  } = project;
 
-    const truncate = (str, len) => str.length > len ? str.slice(0, len) + '...' : str;
-    
-    return (
-        <div className='border shadow-2xl hover:bg-amber-300'>
-            <h1><Link href={`/projects/${id}`}>{title}</Link></h1>
+  const truncate = (str, len) =>
+    str.length > len ? str.slice(0, len) + "..." : str;
 
-            <p>by <Link href={`/profile/${owner_data.username}`}>{owner_data.username}</Link></p>
+  return (
+    <div className="border shadow-2xl hover:bg-amber-300">
+      <h1>
+        <Link href={`/projects/${project_id}`}>{title}</Link>
+      </h1>
 
-            <p>{truncate(description, 100)}</p>
+      <p>
+        by{" "}
+        <Link href={`/profile/${owner_data.username}`}>
+          {owner_data.username}
+        </Link>
+      </p>
 
-            <div className='flex gap-2'>
-                {tech_stack_list.includes('javascript') &&
-                tech_stack_list.map((tech) => (
-                    <TechStackTag key={tech} techStack={tech} />
-                ))
-                }
+      <p>{truncate(description, 100)}</p>
 
-                {roles_list.length > 0 &&
-                roles_list.map((role) => (
-                    <RoleTag key={role} role={role} />
-                ))
-                }
-            </div>
+      <div className="flex gap-2">
+        {tech_stack_list.includes("javascript") &&
+          tech_stack_list.map((tech) => (
+            <TechStackTag key={tech} techStack={tech} />
+          ))}
 
-            <footer className='text-sm'>
-                {new Date(created_at).toLocaleDateString()}
-            </footer>
-        </div>
-    );
+        {roles_list.length > 0 &&
+          roles_list.map((role) => <RoleTag key={role} role={role} />)}
+      </div>
+
+      <footer className="text-sm">
+        {new Date(created_at).toLocaleDateString()}
+      </footer>
+    </div>
+  );
 }
 
 export default ProjectCard;
