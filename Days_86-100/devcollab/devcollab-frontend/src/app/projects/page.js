407841d0e8e7c1f854e211ff7c54710e16bfe90a
saad299 <msaadahmad421@gmail.com>
2026-06-22 01:58:30 +0500

    DevCollab: nothing just a small key map issue solved in projects page

--- Days_86-100/devcollab/devcollab-frontend/src/app/projects/page.js
+++ Days_86-100/devcollab/devcollab-frontend/src/app/projects/page.js
@@ -180,7 +180,7 @@ function ProjectsPage() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {projects.map((project) => (
               <ProjectCard
-                key={project.id}
+                key={project.project_id}
                 project={project}
               />
             ))}
