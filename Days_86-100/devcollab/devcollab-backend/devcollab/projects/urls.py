9e4e37382e9599ce9a20a5f96c47fe59891b5699
saad299 <msaadahmad421@gmail.com>
2026-08-09 01:55:45 +0500

    DevCollab: cleared up unnecessary lines of code

--- Days_86-100/devcollab/devcollab-backend/devcollab/projects/urls.py
+++ Days_86-100/devcollab/devcollab-backend/devcollab/projects/urls.py
@@ -9,30 +9,6 @@ router.register(
     basename='project'
 )
 
-# {"id":6,"username":"Saad","email":"saad@gamil.com","profile":{"id":6,"bio":null,"avatar":null,"skills":null,"github_url":null,"linkedin_url":null,"website_url":null,"location":null,"user":6}}
-"""
-1.
-username = Saad
-email = saad@gamil.com
-password = ewrew@#$@1432rrew
-2.
-username = Saad1
-email = saad1@gamil.com
-password = erwETE25@$$@fdsfr
-3.
-username = Saad2
-email = saad2@gamil.com
-password =  ffwSd@#$@124:s35!#$s
-4.
-username = Some_User_123
-email = someuser@devcollab.com
-password =   devcollab_User_12345
-5.
-username = Some_User_456
-email = some_user_again@devcollab.com
-password =    devcollab_User_34567
-"""
-
 urlpatterns = [
     # Project URLs - handled by router
     # GET    /api/projects/              — list all open projects
