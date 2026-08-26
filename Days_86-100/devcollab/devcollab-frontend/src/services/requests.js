fee77e2cc0fe0dd90ca108774d3ed874b649bb2d
saad299 <msaadahmad421@gmail.com>
2026-08-27 01:24:22 +0500

    DevCollab: fixed the requests file so that the collab request could come correctly to dashboard

--- Days_86-100/devcollab/devcollab-frontend/src/services/requests.js
+++ Days_86-100/devcollab/devcollab-frontend/src/services/requests.js
@@ -2,23 +2,23 @@ import api from './api'
 
 
 export async function sendRequest(projectId, message) {
-    response = await api.post(`/projects/${projectId}/requests/`, { message })
+    const response = await api.post(`/projects/${projectId}/requests/`, { message })
     return response.data
 }
 
 
 export async function getProjectRequests(projectId) {
-    response = await api.get(`/projects/${projectId}/requests/`)
+    const response = await api.get(`/projects/${projectId}/requests/`)
     return response.data
 }
 
 
 export async function updateRequestStatus(projectId, requestId, newStatus) {
-    response = await api.patch(`/projects/${projectId}/requests/${requestId}/`, { status: newStatus })
+    const response = await api.patch(`/projects/${projectId}/requests/${requestId}/`, { status: newStatus })
     return response.data
 }
 
 export async function getMyRequests() {
-    response = await api.get(`/requests/mine/`)
+    const response = await api.get(`/requests/mine/`)
     return response.data
 }
