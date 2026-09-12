3b6fb1f5074aadfe9c6db307b6ee83ef65aa3bab
saad299 <msaadahmad421@gmail.com>
2026-09-12 07:42:57 +0500

    DevCollab: changed the formatting of the Navbar.js file

--- Days_86-100/devcollab/devcollab-frontend/src/components/layout/Navbar.js
+++ Days_86-100/devcollab/devcollab-frontend/src/components/layout/Navbar.js
@@ -139,7 +139,7 @@ function Navbar() {
                 </Link>
                 <Link
                   href="/projects/new"
-                  className="text-[14px] text-gray-600 px-[10px] py-[8px] rounded-md cursor-pointer hover:bg-gray-50 hover:text-gray-900 transition-colors"
+                  className="text-[14px] text-gray-600 px-2.5 py-2 rounded-md cursor-pointer hover:bg-gray-50 hover:text-gray-900 transition-colors"
                   onClick={() => setMobileMenuOpen(false)}
                 >
                   Post Project
